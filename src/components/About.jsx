8a2798378ac2b7b02665a9a8c7979767ba15d922
ELIE-MANI <eliemaniraguha@gmail.com>
2026-05-27 15:07:05 +0200

    Remove duplicate nested React app

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,10 +9,12 @@ function About (){
             <div className="flex justify-center items-center ">
                 <img src={Me} alt="Elie" className="w-48 h-58  mx-auto mb-6 object-cover"/>
                 <div>
-                 <p className="text-lg leading-relaxed text-gray-700 max-w-3xl mx-auto">
-                    Hello! I'm Elie, a passionate web developer with a knack for creating beautiful and functional websites.
-                     With a strong foundation in HTML, CSS, JavaScript & React. <br /> I enjoy bringing ideas to life in the digital world. When I'm not coding, you can find me exploring new technologies or working on personal projects to enhance my skills.
-                </p>
+                 <p className="text-lg leading-relaxed text-gray-700 max-w-3xl mx-auto ">
+                    Hello! I'm Elie, a passionate Full-Stack Developer and UI/UX Designer dedicated to building modern, responsive, and user-friendly web applications.
+                    With strong experience in React, JavaScript, Tailwind CSS, Node.js, Express.js, PostegreSQL, and Figma, I enjoy transforming ideas into real digital experiences. <br />
+                    I’m constantly learning new technologies, improving my development skills, and creating projects that combine clean design with scalable functionality.
+                 </p>
+                    
                 <a href="https://docs.google.com/document/d/14v1PB_RCBxmcLoFTVKyfVz87Qz-Sq1Yvdfr-69I_uGI/edit?usp=sharing" target="_blank" rel="noopener noreferrer">
                     <button className="mt-6 px-6 py-3 bg-blue-600 text-white rounded-lg cursor-pointer">Download CV</button>
                 </a>
